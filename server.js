4049c5203a0157996b8f75a2e89480e3d929c46d
obrailovska <olabrrai@icloud.com>
2022-05-01 11:07:07 -0400

    final product

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3002;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-app";
 
 const app = express();
 
